68661ecc55db9d29d390fc3a2d62fa2a75fe417d
YuLin Chang <511421412@qq.com>
2019-07-05 23:43:21 +0800

    Add MomPoint

---
+++ Assets/Scripts/MomPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomPoint : MonoBehaviour
+{
+    public GameObject enemyPfb;
+    public float generateCD;
+    public int totalCount;
+
+    private float lastGeneTick;
+    private int hasGene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasGene < totalCount
+            && (lastGeneTick <= 0 || lastGeneTick + generateCD <= Time.time))
+            GenerateEnemy();
+    }
+
+    void GenerateEnemy()
+    {
+        GameObject newEnemy = Instantiate(enemyPfb);
+        newEnemy.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
+        lastGeneTick = Time.time;
+        hasGene++;
+    }
+}
