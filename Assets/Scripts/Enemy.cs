45f291d6c7b91309835c49053bed67053e3be582
fuyingdi <fuyingdi@126.com>
2019-07-05 23:08:19 +0800

    folder

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject target;
+
+    public 
+
+    void Start()
+    {
+        target = GameObject.Find("Player");
+        if(target == null)
+        {
+            Debug.Log("can't find target Player");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+    
+
+
+    void ChasePlayer()
+    {
+        MoveTo();
+    }
+    
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rg;
+    public float speed = 10f;
+
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+        MoveUpdate();
+    }
+
+
+    // 控制移动
+    void MoveUpdate()
+    {
+        Vector2 movedir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Debug.Log("movedir:" + movedir);
+        if (movedir != new Vector2(0, 0))
+        {
+            rg.velocity = movedir * speed;
+        }
+    }
+}
