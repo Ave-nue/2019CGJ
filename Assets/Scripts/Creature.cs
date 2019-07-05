f5bcfa0f1d42245d5bf5f12d94606114d298efbd
YuLin Chang <511421412@qq.com>
2019-07-05 23:01:31 +0800

    ADD creature class

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    [SerializeField]
+    float velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void Move(Vector2 direction)
+    {
+        transform.Translate(direction * velocity * Time.deltaTime);
+    }
+
+    protected void MoveTo(Vector3 target)
+    {
+
+    }
+}
